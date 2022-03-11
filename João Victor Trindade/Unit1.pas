746af935e775e4a874573ff3be607575bd3c5437
trndde <68303932+trndde@users.noreply.github.com>
2022-03-11 12:00:31 -0300

    Add files via upload

---
+++ João Victor Trindade/Unit1.pas
@@ -0,0 +1,232 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Mask;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    cb1: TCheckBox;
+    cb2: TCheckBox;
+    Edit1: TEdit;
+    ComboBox1: TComboBox;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label2: TLabel;
+    cb3: TCheckBox;
+    cb4: TCheckBox;
+    cb5: TCheckBox;
+    Edit4: TEdit;
+    Label3: TLabel;
+    bSalvar: TButton;
+    bRelatorio: TButton;
+    MaskEdit1: TMaskEdit;
+    MaskEdit2: TMaskEdit;
+    procedure bRelatorioClick(Sender: TObject);
+    procedure bSalvarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Edit1Enter(Sender: TObject);
+    procedure Edit4Enter(Sender: TObject);
+
+
+  private
+    { Private declarations }
+    vNome: String;
+    vTelefone: String;
+    vMarca: String;
+    vIndice: Integer;
+    vModelo: String;
+    vImei: String;
+    vPrioridade: String;
+    vData: String;
+    vRequerimento: String;
+    vDescricao: String;
+    wStringList: TStringList;
+
+
+    function fDiretorio: String;
+    function fRelatorio: String;
+    procedure pSalvar;
+
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+//SCRIPT
+
+function TForm1.fRelatorio;
+begin
+vNome := Edit1.text;
+vTelefone:= MaskEdit1.Text;
+vData := MaskEdit2.Text;
+vModelo := Edit2.Text;
+vImei := Edit3.Text;
+vDescricao:= Edit4.Text;
+
+
+vIndice:= ComboBox1.ItemIndex;
+  if vIndice = 0 then
+    vMarca := 'Apple';
+  if vIndice = 1 then
+    vMarca:= 'Asus';
+  if vIndice = 2 then
+    vMarca:= 'Lenovo';
+  if vIndice = 3 then
+    vMarca:= 'LG';
+  if vIndice = 4 then
+    vMarca:= 'Microsoft';
+  if vIndice = 5 then
+    vMarca:= 'Motorola';
+  if vIndice = 6 then
+    vMarca:= 'Multilaser';
+  if vIndice = 7 then
+    vMarca:= 'Nokia';
+  if vIndice = 8 then
+    vMarca:= 'Samsung';
+  if vIndice = 9 then
+    vMarca:= 'Sony';
+  if vIndice = 10 then
+    vMarca:= 'Xiaomi';
+  if vIndice = 11 then
+    vMarca:= 'Outros';
+
+
+  if cb3.Checked then
+    vPrioridade:= 'Baixa'
+  else if cb4.Checked then
+    vPrioridade:= 'Média'
+  else if cb5.Checked then
+    vPrioridade:= 'Alta';
+
+
+  if cb1.Checked then
+     vRequerimento:= 'Pedido'
+  else if cb2.Checked then
+     vRequerimento:= 'Orçamento';
+
+Result:= ('Nota de ' + vRequerimento + #13 + 'Nome do cliente: ' + vNome + #13 + 'Telefone: ' + vTelefone + #13 + 'Marca: ' + vMarca + #13 + 'Modelo: ' + vModelo + #13 + 'IMEI: ' + vImei + #13 + 'Prioridade: ' + vPrioridade + #13 + 'Descrição do defeito: ' + vDescricao + #13 + 'Data: ' + vData)
+end;
+
+
+procedure TForm1.pSalvar;
+begin
+vNome := Edit1.text;
+vTelefone:= MaskEdit1.Text;
+vData := MaskEdit2.Text;
+vModelo := Edit2.Text;
+vImei := Edit3.Text;
+vDescricao:= Edit4.Text;
+
+
+vIndice:= ComboBox1.ItemIndex;
+  if vIndice = 0 then
+    vMarca := 'Apple';
+  if vIndice = 1 then
+    vMarca:= 'Asus';
+  if vIndice = 2 then
+    vMarca:= 'Lenovo';
+  if vIndice = 3 then
+    vMarca:= 'LG';
+  if vIndice = 4 then
+    vMarca:= 'Microsoft';
+  if vIndice = 5 then
+    vMarca:= 'Motorola';
+  if vIndice = 6 then
+    vMarca:= 'Multilaser';
+  if vIndice = 7 then
+    vMarca:= 'Nokia';
+  if vIndice = 8 then
+    vMarca:= 'Samsung';
+  if vIndice = 9 then
+    vMarca:= 'Sony';
+  if vIndice = 10 then
+    vMarca:= 'Xiaomi';
+  if vIndice = 11 then
+    vMarca:= 'Outros';
+
+
+  if cb3.Checked then
+    vPrioridade:= 'Baixa'
+  else if cb4.Checked then
+    vPrioridade:= 'Média'
+  else if cb5.Checked then
+    vPrioridade:= 'Alta';
+
+
+  if cb1.Checked then
+     vRequerimento:= 'Pedido'
+  else if cb2.Checked then
+     vRequerimento:= 'Orçamento';
+
+wStringList.Add('Nota de ' + vRequerimento + #13 + 'Nome do cliente: ' + vNome + #13 + 'Telefone: ' + vTelefone + #13 + 'Marca: ' + vMarca + #13 + 'Modelo: ' + vModelo + #13 + 'IMEI: ' + vImei + #13 + 'Prioridade: ' + vPrioridade + #13 + 'Descrição do defeito:' + vDescricao + #13 + 'Data: ' + vData +#13+#13)
+end;
+
+
+procedure TForm1.bRelatorioClick(Sender: TObject);
+begin
+ShowMessage(fRelatorio);
+end;
+
+
+
+
+function Tform1.fDiretorio: String;
+begin
+  Result:= 'c:\BackupOrçamentos\Orçamentos.txt'
+end;
+
+
+
+procedure TForm1.bSalvarClick(Sender: TObject);
+  var
+    vContador: integer;
+    vTexto: String;
+begin
+pSalvar;
+  for vContador := 0 to wStringList.Count-1 do
+    vTexto := wStringList[vContador];
+
+
+    if not DirectoryExists('c:\BackupOrçamentos') then
+      ForceDirectories('c:\BackupOrçamentos');
+
+    wStringList.SaveToFile(fDiretorio);
+
+end;
+
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+wStringList:= TStringList.Create;
+ if FileExists('c:\BackupOrçamentos\Orçamentos.txt') then
+   wStringList.LoadFromFile('c:\BackupOrçamentos\Orçamentos.txt');
+end;
+
+procedure TForm1.Edit1Enter(Sender: TObject);
+begin
+edit1.text:= '' 
+end;
+
+procedure TForm1.Edit4Enter(Sender: TObject);
+begin
+Edit4.text:=''
+end;
+
+end.
+
+
+//Me contrata aí, pô :) 
+
+
+
---
+++ João Victor Trindade/Unit1.~dfm
@@ -0,0 +1,227 @@
+object Form1: TForm1
+  Left = 203
+  Top = 148
+  Width = 466
+  Height = 439
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 96
+    Top = 8
+    Width = 228
+    Height = 28
+    Caption = 'OR'#199'AMENTOweb 1.0'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clMaroon
+    Font.Height = -21
+    Font.Name = 'MV Boli'
+    Font.Style = [fsBold]
+    ParentFont = False
+    Layout = tlCenter
+  end
+  object Label2: TLabel
+    Left = 336
+    Top = 56
+    Width = 29
+    Height = 16
+    Caption = 'Data'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object Label3: TLabel
+    Left = 16
+    Top = 168
+    Width = 67
+    Height = 16
+    Caption = 'Prioridade'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object Label4: TLabel
+    Left = 224
+    Top = 168
+    Width = 31
+    Height = 16
+    Caption = 'IMEI:'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object cb1: TCheckBox
+    Left = 16
+    Top = 56
+    Width = 97
+    Height = 17
+    Caption = 'PEDIDO'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+    TabOrder = 0
+  end
+  object cb2: TCheckBox
+    Left = 128
+    Top = 56
+    Width = 121
+    Height = 17
+    Caption = 'OR'#199'AMENTO'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+    TabOrder = 1
+  end
+  object Edit1: TEdit
+    Left = 16
+    Top = 96
+    Width = 297
+    Height = 21
+    TabOrder = 2
+    Text = 'Nome '
+    OnEnter = Edit1Enter
+  end
+  object Edit2: TEdit
+    Left = 336
+    Top = 128
+    Width = 97
+    Height = 21
+    TabOrder = 3
+    Text = 'Modelo'
+  end
+  object ComboBox1: TComboBox
+    Left = 16
+    Top = 128
+    Width = 297
+    Height = 21
+    ItemHeight = 13
+    TabOrder = 4
+    Text = 'Marca'
+    Items.Strings = (
+      'Apple'
+      'Asus'
+      'Lenovo'
+      'LG'
+      'Microsoft'
+      'Motorola'
+      'Multilaser'
+      'Nokia'
+      'Samsung'
+      'Sony'
+      'Xiaomi'
+      'Outros...')
+  end
+  object MaskEdit1: TMaskEdit
+    Left = 336
+    Top = 96
+    Width = 97
+    Height = 21
+    EditMask = '!\(99\)#0000-0000;1;_'
+    MaxLength = 14
+    TabOrder = 5
+    Text = '(  )     -    '
+  end
+  object MaskEdit2: TMaskEdit
+    Left = 376
+    Top = 56
+    Width = 57
+    Height = 21
+    EditMask = '!99/99/00;1;_'
+    MaxLength = 8
+    TabOrder = 6
+    Text = '  /  /  '
+  end
+  object cb3: TCheckBox
+    Left = 16
+    Top = 192
+    Width = 97
+    Height = 17
+    Caption = 'Baixa'
+    TabOrder = 7
+  end
+  object cb4: TCheckBox
+    Left = 72
+    Top = 192
+    Width = 97
+    Height = 17
+    Caption = 'M'#233'dia'
+    TabOrder = 8
+  end
+  object cb5: TCheckBox
+    Left = 128
+    Top = 192
+    Width = 97
+    Height = 17
+    Caption = 'Alta'
+    TabOrder = 9
+  end
+  object Edit4: TEdit
+    Left = 16
+    Top = 224
+    Width = 417
+    Height = 21
+    TabOrder = 10
+    Text = 'Descreva o defeito...'
+    OnEnter = Edit4Enter
+  end
+  object bSalvar: TButton
+    Left = 16
+    Top = 256
+    Width = 137
+    Height = 65
+    Caption = 'Salvar'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+    TabOrder = 11
+    OnClick = bSalvarClick
+  end
+  object bRelatorio: TButton
+    Left = 296
+    Top = 256
+    Width = 137
+    Height = 65
+    Caption = 'Relat'#243'rio'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clBlack
+    Font.Height = -13
+    Font.Name = 'Arial'
+    Font.Style = [fsBold]
+    ParentFont = False
+    TabOrder = 12
+    OnClick = bRelatorioClick
+  end
+  object Edit3: TEdit
+    Left = 224
+    Top = 192
+    Width = 209
+    Height = 21
+    TabOrder = 13
+  end
+end
